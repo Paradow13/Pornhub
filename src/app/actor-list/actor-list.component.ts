aedfdead4a4763bdf13272d27d02155072aca4b0
Paradow <desantoine.joffrey@hotmail.com>
2019-07-01 19:14:14 +0200

    Init project: Add, Read, delete done

--- src/app/actor-list/actor-list.component.ts
+++ src/app/actor-list/actor-list.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { Actor } from '../util/actor';
-import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+import {Component, OnInit} from '@angular/core';
+import {Actor} from '../util/actor';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import {first, last} from 'rxjs/operators';
+import {log} from 'util';
 
 @Component({
   selector: 'app-actor-list',
@@ -9,57 +11,74 @@ import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 })
 export class ActorListComponent implements OnInit {
 
-  title = 'css-angular';
-  closeResult: string;
-
   private _actorList: Actor[] = [];
+
+  tmpActor = new Actor('', '', '');
+
   public tmpFirstName: string;
   public tmpLastName: string;
   public tmpLinkGoole: string;
 
-  constructor(private modalService: NgbModal) {}
+  public tmp;
 
-  ngOnInit() {
-    this._actorList.push(new Actor("Rone","Samantha", "https://www.google.com/search?q=Samantha%20Rone"));
-    this._actorList.push(new Actor("London","Layla", "https://www.google.com/search?q=Layla%20London"));
-    this._actorList.push(new Actor("Reid","Riley", "https://www.google.com/search?q=Riley%20Reid"));
+  constructor(private modalService: NgbModal) {
   }
 
-  public deleteActor(firstName:String, lastName:String): void{
-    for(let actor of this._actorList){
-      if(actor.lastName == lastName && actor.firstName == firstName) {
-        this._actorList.splice(this._actorList.indexOf(actor),1);
+  ngOnInit() {
+    this._actorList.push(new Actor('Rone', 'Samantha', 'https://www.google.com/search?q=Samantha%20Rone'));
+    this._actorList.push(new Actor('London', 'Layla', 'https://www.google.com/search?q=Layla%20London'));
+    this._actorList.push(new Actor('Reid', 'Riley', 'https://www.google.com/search?q=Riley%20Reid'));
+  }
+
+  public setFieldsToNul() {
+    this.tmpFirstName = null;
+    this.tmpLastName = null;
+    this.tmpLinkGoole = null;
+  }
+
+  public deleteActor(firstName: String, lastName: String): void {
+    for (let actor of this._actorList) {
+      if (actor.lastName == lastName && actor.firstName == firstName) {
+        this._actorList.splice(this._actorList.indexOf(actor), 1);
       }
     }
   }
 
-  public addActor():void {
+  public addActor(): void {
     if (this.tmpFirstName.length != 0 && this.tmpLastName.length != 0 && this.tmpLinkGoole.length != 0) {
       this._actorList.push(new Actor(this.tmpLastName, this.tmpFirstName, this.tmpLinkGoole));
-      this.tmpFirstName = "";
-      this.tmpLastName = "";
-      this.tmpLinkGoole = "";
+      this.tmpFirstName = '';
+      this.tmpLastName = '';
+      this.tmpLinkGoole = '';
 
     } else {
-      alert("One field is empty");
+      alert('One field is empty');
     }
   }
 
   open(content) {
-    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
-      this.closeResult = `Closed with: ${result}`;
-    }, (reason) => {
-      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-    });
+    this.setFieldsToNul();
+    this.modalService.open(content);
   }
 
-  private getDismissReason(reason: any): string {
-    if (reason === ModalDismissReasons.ESC) {
-      return 'by pressing ESC';
-    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-      return 'by clicking on a backdrop';
-    } else {
-      return  `with: ${reason}`;
+  setDeleteTmp(firstName: string, lastName: string) {
+    this.tmpLastName = lastName;
+    this.tmpFirstName = firstName;
+  }
+
+  setUpdateTmp(firstName: string, lastName: string, linkGoogle: string) {
+    this.tmpActor.firstName = firstName;
+    this.tmpActor.lastName = lastName;
+    this.tmpActor.linkGoogle = linkGoogle;
+  }
+
+  updateActor(firstName: string, lastName: string, newActor: Actor) {
+    for (let actor of this._actorList) {
+      if (actor.lastName == lastName && actor.firstName == firstName) {
+        this._actorList[this._actorList.indexOf(actor)].lastName = newActor.lastName;
+        this._actorList[this._actorList.indexOf(actor)].firstName = newActor.firstName;
+        this._actorList[this._actorList.indexOf(actor)].linkGoogle = newActor.linkGoogle;
+      }
     }
   }
 }
